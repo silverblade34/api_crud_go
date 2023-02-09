677c430cf1f031a2371eb45877de905290ce923a
Michell Pacheco Tacay <90942815+silverblade34@users.noreply.github.com>
2023-02-09 10:18:34 -0500

    distribuido en repository

--- go.mod
+++ go.mod
@@ -3,6 +3,29 @@ module api-v2
 go 1.16
 
 require (
-	github.com/gin-gonic/gin v1.6.3
-	go.mongodb.org/mongo-driver v1.6.0
+	github.com/cespare/reflex v0.3.1 // indirect
+	github.com/creack/pty v1.1.18 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/gin-gonic/gin v1.8.2
+	github.com/go-playground/validator/v10 v10.11.2 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/gobuffalo/genny v0.1.1 // indirect
+	github.com/gobuffalo/gogen v0.1.1 // indirect
+	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/karrick/godirwalk v1.10.3 // indirect
+	github.com/klauspost/compress v1.15.15 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/montanaflynn/stats v0.7.0 // indirect
+	github.com/pelletier/go-toml v1.7.0 // indirect
+	github.com/sirupsen/logrus v1.4.2 // indirect
+	github.com/ugorji/go/codec v1.2.9 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
+	go.mongodb.org/mongo-driver v1.11.1
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 )
--- repository/user_repository.go
+++ repository/user_repository.go
@@ -1,7 +1,7 @@
 package repository
 
 import (
-	model "api-v2/model"
+	m "api-v2/model"
 	"context"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -9,31 +9,51 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// UserRepository representa el repositorio de usuarios.
-type UserRepository struct {
+// UserRepository es una interfaz para la persistencia de usuarios.
+type UserRepository interface {
+	ValidUser(email string, password string) (*m.User, error)
+}
+
+// UserMongoRepository es una implementación de la interfaz UserRepository que utiliza MongoDB como almacenamiento.
+type UserMongoRepository struct {
 	client *mongo.Client
 }
 
-// NewUserRepository crea una nueva instancia de UserRepository.
-func NewUserRepository() (*UserRepository, error) {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		return nil, err
-	}
-	err = client.Connect(context.TODO())
-	if err != nil {
-		return nil, err
-	}
-	return &UserRepository{client: client}, nil
+// NewUserMongoRepository crea una nueva instancia de UserMongoRepository.
+func NewUserMongoRepository(client *mongo.Client) *UserMongoRepository {
+	return &UserMongoRepository{client: client}
 }
 
-// GetByEmailAndPassword obtiene un usuario por su email y password.
-func (r *UserRepository) GetByEmailAndPassword(email, password string) (*model.User, error) {
+// ValidUser verifica si un usuario existe en la base de datos.
+func (r *UserMongoRepository) ValidUser(email, password string) (*m.User, error) {
+	// Obtiene la colección "users_test" de la base de datos "test".
 	collection := r.client.Database("test").Collection("users_test")
-	var user model.User
+	// Estructura que se usará para almacenar el usuario que se obtiene de la base de datos.
+	var user m.User
+	// Busca el usuario en la base de datos.
 	err := collection.FindOne(context.TODO(), bson.M{"email": email, "password": password}).Decode(&user)
 	if err != nil {
 		return nil, err
 	}
 	return &user, nil
+}
+
+func InsertUser(user m.User) error {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		return err
+	}
+	err = client.Connect(context.TODO())
+	if err != nil {
+		return err
+	}
+	defer client.Disconnect(context.TODO())
+
+	collection := client.Database("test").Collection("users_test")
+
+	_, err = collection.InsertOne(context.TODO(), user)
+	if err != nil {
+		return err
+	}
+	return nil
 }
